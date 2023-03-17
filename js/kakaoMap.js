53bb03983a5a76813241fd9a121d05deb0c8ef3d
신승헌 <ssh5212@naver.com>
2023-03-17 17:48:16 +0900

    error: location.html / 에러 발생

--- js/kakaoMap.js
+++ js/kakaoMap.js
@@ -1,3 +1,5 @@
+let contentTypeId = 0; // 0이면 다 고르는 것
+
 // 검색 버튼 눌렸을 때 실행
 document.getElementById("btn-search").addEventListener("click", () => {
     // 검색 기본 url
@@ -5,7 +7,8 @@ document.getElementById("btn-search").addEventListener("click", () => {
 
     // 추가 검색 속성값 적용
     let areaCode = document.getElementById("search-area").value;
-    let contentTypeId = document.getElementById("search-content-id").value;
+    contentTypeId = document.getElementById("search-content-id").value;
+    // console.log("contentT" + contentTypeId);
     let keyword = document.getElementById("search-keyword").value;
 
     if (parseInt(areaCode)) searchUrl += `&areaCode=${areaCode}`;
@@ -28,6 +31,7 @@ function makeList(data) {
     let tripList = ``;
     positions = [];
     trips.forEach((area) => {
+        // console.log(area);
         tripList += `
                     <tr onclick="moveCenter(${area.mapy}, ${area.mapx});">
                         <td><img src="${area.firstimage}" width="100px"></td>
@@ -44,6 +48,7 @@ function makeList(data) {
             latlng: new kakao.maps.LatLng(area.mapy, area.mapx),
             contenttypeid: area.contenttypeid,
             addr: area.addr1,
+            image: area.firstimage,
         };
         positions.push(markerInfo);
     });
@@ -75,12 +80,24 @@ function displayMarker() {
         // 마커 이미지를 생성합니다
         var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
+        //////////////// error
+        console.log();
+
+        if (contentTypeId != 0 && contentTypeId != positions[i].contenttypeid) {
+            continue;
+        }
+        
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
             map: map, // 마커를 표시할 지도
             position: positions[i].latlng, // 마커를 표시할 위치
             title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             image: markerImage, // 마커 이미지
+        });
+        kakao.map.event.addEventListener("click", marker, () => {
+            querySelector("#map_modal_title").innerText(positions[i].title);
+            querySelector("#map_modal_addr").innerText(positions[i].addr);
+            querySelector("#map_modal_img").src(positions[i].image);
         });
     }
 
@@ -91,3 +108,26 @@ function displayMarker() {
 function moveCenter(lat, lng) {
     map.setCenter(new kakao.maps.LatLng(lat, lng));
 }
+
+//////////////////////// 클러스터러 만들기
+// // 마커 클러스터러를 생성합니다 
+// var clusterer = new kakao.maps.MarkerClusterer({
+//     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+//     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+//     minLevel: 10 // 클러스터 할 최소 지도 레벨 
+// });
+
+// // 데이터를 가져오기 위해 jQuery를 사용합니다
+// // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
+// $.get("/download/web/data/chicken.json", function(data) {
+//     // 데이터에서 좌표 값을 가지고 마커를 표시합니다
+//     // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+//     var markers = $(data.positions).map(function(i, position) {
+//         return new kakao.maps.Marker({
+//             position : new kakao.maps.LatLng(position.lat, position.lng)
+//         });
+//     });
+
+//     // 클러스터러에 마커들을 추가합니다
+//     clusterer.addMarkers(markers);
+// });
