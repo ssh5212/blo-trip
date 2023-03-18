160c081b28b772f64a517b0db5520bca00bc661e
김주하 <dnsfudcjsfud@gmail.com>
2023-03-19 00:02:57 +0900

    fix: 00:02

--- js/data.js
+++ js/data.js
@@ -3,3 +3,16 @@ window.onload = init();
 function init() {
     
 }
+
+// 마이페이지에서 쓰는 함수 두 개입니당
+function goModify(){
+    console.log("수정페이지");
+    document.getElementById("removeId").style.display = "none";
+    document.getElementById("modifyContents").style.display = "contents";
+}
+
+function goDelete(){
+    console.log("삭제페이지");
+    document.getElementById("removeId").style.display = "contents";
+    document.getElementById("modifyContents").style.display = "none";
+}
